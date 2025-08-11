dae4038c432b44e354f124a059f25de8f0931a99
Farya <farya.hemati1313@gmail.com>
2025-08-12 02:36:18 +0330

    update Emails with the table's fields and setters and getters

--- src/main/java/aut/ap/Emails.java
+++ src/main/java/aut/ap/Emails.java
@@ -1,9 +1,65 @@
 package aut.ap;
 
-
 import jakarta.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Table( name = "Emails" )
 public class Emails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Basic( optional = false )
+    private String sender, recipient, subject, body, code;
+
+    private boolean isRead;
+    private LocalDate timestamp;
+
+
+    public void setSender(String sender){
+        this.sender = sender;
+    }
+    public String getSender(){
+        return sender;
+    }
+
+    public void setRecipient(String recipient){
+        this.recipient = recipient;
+    }
+    public String getRecipient(){
+        return recipient;
+    }
+
+    public void setSubject(String subject){
+        this.subject = subject;
+    }
+    public String getSubject(){
+        return subject;
+    }
+
+    public void setBody(String body){
+        this.body = body;
+    }
+    public String getBody(){
+        return body;
+    }
+
+    public void setCode(String code){
+        this.code = code;
+    }
+    public String getCode(){
+        return code;
+    }
+
+    public void setRead(boolean isRead){
+        this.isRead = isRead;
+    }
+
+    public void setTimestamp(LocalDate timestamp){
+        this.timestamp = timestamp;
+    }
+    public LocalDate getTimestamp(){
+        return timestamp;
+    }
 }
