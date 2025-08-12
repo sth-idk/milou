45a72c6a9a349bad565743d8c18b3666bb997521
Farya <farya.hemati1313@gmail.com>
2025-08-12 03:53:07 +0330

    add signIn method to Main.java

--- src/main/java/aut/ap/Main.java
+++ src/main/java/aut/ap/Main.java
@@ -3,11 +3,13 @@ package aut.ap;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import java.time.LocalDateTime;
+
 import java.util.*;
 
 public class Main {
 
+    private static final Scanner scanner = new Scanner(System.in);
+    private static Users alreadyLoggedin;
     private static SessionFactory sessionFactory;
     private static void setUpSessionFactory() {
         sessionFactory = new Configuration()
@@ -23,14 +25,65 @@ public class Main {
 
     public static void main(String[] args){
         setUpSessionFactory();
-        Session session = sessionFactory.openSession();
 
 
 
 
 
-        session.close();
+
         closeSessionFactory();
     }
+
+
+
+    private static void signIn() {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        System.out.print("first name: ");
+        String firstName = scanner.nextLine();
+        System.out.print("last name: ");
+        String lastName = scanner.nextLine();
+        System.out.print("age: ");
+        Integer age = Integer.parseInt(scanner.nextLine());
+        System.out.print("email: ");
+        String email = scanner.nextLine().trim().toLowerCase();
+        System.out.print("password: ");
+        String password = scanner.nextLine();
+
+        if (password.length() < 8) {
+            System.out.println("weak password, it must be at least 8 characters.");
+            session.getTransaction().commit();
+            session.close();
+            return;
+        }
+
+        boolean exists = false;
+        List<Users> users = session.createQuery("FROM User", Users.class).list();
+        for (Users user : users) {
+            if (user.getEmail().equalsIgnoreCase(email)) {
+                exists = true;
+                break;
+            }
+        }
+
+        if (exists) {
+            System.out.println("an account with this email already exists");
+        } else {
+            Users newUser = new Users();
+            newUser.setFirstName(firstName);
+            newUser.setLastName(lastName);
+            newUser.setAge(age);
+            newUser.setEmail(email);
+            newUser.setPassword(password);
+            session.persist(newUser);
+            System.out.println("your new account is created. go ahead and login!");
+        }
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+
 
 }
